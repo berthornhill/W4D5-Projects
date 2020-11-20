fa0d612e0ae2a9e95f78e8af565c931b7eaaa293
berthornhill <64211468+berthornhill@users.noreply.github.com>
2020-11-20 15:39:18 -0500

    Completed execution_and_time_diffs

--- execution_and_time_diffs.rb
+++ execution_and_time_diffs.rb
@@ -12,7 +12,7 @@ def my_min(list)
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)  # =>  -5
+ my_min(list)  # =>  -5
 
 def better_min(list)
     list.inject(0) do |small, ele|
@@ -24,9 +24,52 @@ def better_min(list)
     end
     
 end
-p better_min(list)  # =>  -5
+ better_min(list)  # =>  -5
 
 
 
+
+def largest_contiguous_subsum(array)
+    
+    total = array.first
+
+    (0...array.length).each do |i1| 
+        (i1...array.length).each do |i2|
+            if array[i1..i2].sum > total
+                total = array[i1..i2].sum
+            end
+        end
+    end 
+    total
+end
+
+
+def super_fast_contiguous_subsum(array) 
+    largest = array[0] 
+    current = array[0] 
+
+    (1...array.length).each do |i| 
+        current = 0 if current < 0 
+        current += array[i] #
+        
+        largest = current if current > largest 
+    end
+    largest
+end
+
+
+
+
+list = [5, 3, -7] #=> 8
+list2 = [2, 3, -6, 7, -6, 7]
+list3 = [-5, -1, -3]
+
+# p largest_contiguous_subsum(list) # => 8
+# p largest_contiguous_subsum(list2) # => 8 (from [7, -6, 7])
+# p largest_contiguous_subsum(list3) # => -1 (from [-1])
+
+p super_fast_contiguous_subsum(list) # => 8
+p super_fast_contiguous_subsum(list2) # => 8 (from [7, -6, 7])
+p super_fast_contiguous_subsum(list3) # => -1 (from [-1])
 
 
