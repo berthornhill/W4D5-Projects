bb447f70fc0056151a3a49ddaad8314c9ce12439
berthornhill <64211468+berthornhill@users.noreply.github.com>
2020-11-20 17:13:59 -0500

    Completed anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -24,3 +24,31 @@ end
 p better_ana?("gizmo", "sally")    #=> false
 p better_ana?("elvis", "lives")    #=> true
 
+def potential_ana?(word1, word2)
+    # alpha = ("a".."z").to_a
+
+    a = word1.chars.sort.join 
+    b = word2.chars.sort.join  
+
+    a == b
+end
+
+p potential_ana?("gizmo", "sally")    #=> false
+p potential_ana?("elvis", "lives")    #=> true
+
+def final_ana?(word1, word2)
+    hashana = Hash.new {|hash, key| hash[key] = [] }
+
+    word1.each_char do |char| 
+        hashana[char] <<  char
+    end
+
+    word2.each_char do |char| 
+        hashana[char] <<  char
+    end
+
+    hashana.values.all? {|val| val.length.even?}
+    
+end
+p final_ana?("gizmo", "sally")    #=> false
+p final_ana?("elvis", "lives")    #=> true
